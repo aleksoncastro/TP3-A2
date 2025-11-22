9280d79cb85cd113fa34339f533a89eb42654762
italo <italobeckmann@gmail.com>
2025-11-22 01:40:40 -0300

    refactor: reorganize project structure and move files to api directory
    feat: add angular project

--- api/Program.cs
+++ api/Program.cs
@@ -1,21 +1,18 @@
-using MediaMatch.Data; // Importante: Namespace onde est� o seu DbContext
+using MediaMatch.Data; 
 using MediaMatch.Services;
 using Microsoft.EntityFrameworkCore;
 
 LoadEnv(Path.Combine(Directory.GetCurrentDirectory(), ".env"));
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Recupera a string de conex�o do arquivo appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// 2. === CONFIGURA��O DO DBCONTEXT ===
-// Injeta o MediaMatchContext no container de servi�os usando SQL Server
 builder.Services.AddDbContext<MediaMatchContext>(options =>
     options.UseSqlServer(connectionString));
 
-// Add services to the container.
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
