570a70d25730ac288b44fdb15da9be0f180abe95
aleksoncastro <aleksontrabalho@gmail.com>
2025-11-25 16:27:49 -0300

    feat(footer): implement footer layout with navigation and social links

--- front/media-match/src/app/components/footer/footer.component.ts
+++ front/media-match/src/app/components/footer/footer.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {}
+export class FooterComponent { 
+  currentYear: number = new Date().getFullYear();
+}
